8f1a2b1862a5f5f9a5352b12c1559fc6aa6a69e9
Emelyanov <86175307+EmelyanovDev@users.noreply.github.com>
2022-08-04 16:20:16 +0300

    Tried refactor interaction

--- Assets/Scripts/Interaction/InteractionMode.cs
+++ Assets/Scripts/Interaction/InteractionMode.cs
@@ -1,8 +1,21 @@
-﻿namespace Interaction
+﻿using UnityEngine;
+
+namespace Interaction
 {
-    public enum InteractionMode
+    public abstract class InteractionMode : MonoBehaviour
     {
-        Button,
-        ScreenTouch,
+        [SerializeField] private float acceptableDistance;
+        
+        public abstract Interactive CheckInteractiveObject();
+        
+        protected Interactive IsInteractive(Collider collider)
+        {
+            if (collider == null) return null;
+            if (collider.TryGetComponent(out Interactive interactable) == false) return null;
+            Vector3 colliderPosition = collider.transform.position;
+            if (Vector3.Distance(transform.position, colliderPosition) < acceptableDistance)
+                return interactable;
+            return null;
+        }
     }
 }
--- Assets/Scripts/Interaction/TouchInteractionMode.cs
+++ Assets/Scripts/Interaction/TouchInteractionMode.cs
@@ -1,9 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 namespace Interaction
 {
-    public class TouchInteractionMode : MonoBehaviour
+    public class TouchInteractionMode : InteractionMode
     {
+        [SerializeField] private float acceptableTouchDifference;
         
+        private InteractionRaycast _interactionRaycast;
+        private Vector2 _touchStartPosition;
+
+        private void Awake()
+        {
+            _interactionRaycast = GetComponent<InteractionRaycast>();
+        }
+
+        public override Interactive CheckInteractiveObject()
+        {
+            if (Input.touchCount <= 0) return null;
+            Touch touch = Input.touches.Last();
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    _touchStartPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    var difference = touch.position - _touchStartPosition;
+                    if (difference.magnitude <= acceptableTouchDifference)
+                    {
+                        var collider = _interactionRaycast.RaycastInTouch(touch.position);
+                        return IsInteractive(collider);
+                        Interact();
+                    }
+                    break;
+            }
+        }
     }
 }
